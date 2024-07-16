19635e1bf4f5d48b1bd0aeb1b44aadf4d7976cd2
Busisanii <92332554+Busisanii@users.noreply.github.com>
2024-07-16 12:25:49 +0200

    added font for side bar

--- AuthenticationProject/WebFrontEnd/src/app/app-routing.module.ts
+++ AuthenticationProject/WebFrontEnd/src/app/app-routing.module.ts
@@ -3,17 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { RegisterComponent } from './register/register.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [  
-   { path: '', redirectTo: '/home', pathMatch: 'full' },
+   { path: '', redirectTo: '/Home', pathMatch: 'full' },
   { path: 'Home', component: HomeComponent },
   { path: 'Register', component: RegisterComponent },
   { path: 'LogIn', component: LogInComponent }
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
